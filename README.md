35e9a657cbb30ed8b8d7049186ddaa341f336f41
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-09-04 23:09:21 +0530

    initial publish
    A portfolio and blogging app built on Django

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio-Blog
+ A portfolio and blogging app built on Django
---
+++ projects/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+    }
+    return render(request, "blog_detail.html", context)
